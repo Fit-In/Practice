5c0f3b4b4fa580178d062f03c789b581ff132dd7
cheewr85 <cheewr85@naver.com>
2021-08-20 13:33:24 +0900

    PRA-16 지원 라이브러리 관련 실습완료

---
+++ Oh/3.3/HelloCompatChallenge/app/src/main/java/techtown/org/hellocompat/MainActivity.java
@@ -0,0 +1,57 @@
+package techtown.org.hellocompat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    // 색깔을 나타낼 TextView를 불러옴
+    private TextView mHelloTextView;
+
+    // color.xml에 저장한 이름을 저장함
+    private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHelloTextView = findViewById(R.id.hello_textview);
+
+        // 값이 이미 존재한다면 사라지지 않도록 넣어둠
+        if(savedInstanceState != null) {
+            mHelloTextView.setTextColor(savedInstanceState.getInt("color"));
+        }
+    }
+
+    public void changeColor(View view) {
+        // 20개 중 랜덤으로 색깔을 선택하게 설정함
+        Random random = new Random();
+        String colorName = mColorArray[random.nextInt(20)];
+        int colorResourceName = getResources().getIdentifier(colorName, "color",getApplicationContext().getPackageName());
+//        int colorRes = getResources().getColor(colorResourceName, this.getTheme());
+        int colorRes = 0;
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+            colorRes = getResources().getColor(colorResourceName, this.getTheme());
+        } else {
+            colorRes = getResources().getColor(colorResourceName);
+        }
+        mHelloTextView.setTextColor(colorRes);
+    }
+
+    // 값이 초기화되지 않도록 저장해둠
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("color", mHelloTextView.getCurrentTextColor());
+    }
+}
