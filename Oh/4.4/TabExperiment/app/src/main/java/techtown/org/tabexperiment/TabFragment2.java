0cb7e1e1603aa7d615e0f669e068bae5ace0933e
cheewr85 <cheewr85@naver.com>
2021-09-03 11:26:56 +0900

    PRA-21 Task CC 완료

---
+++ Oh/4.4/TabExperiment/app/src/main/java/techtown/org/tabexperiment/TabFragment2.java
@@ -0,0 +1,28 @@
+package techtown.org.tabexperiment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TabFragment2 extends Fragment {
+
+
+
+    public TabFragment2() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab2, container, false);
+    }
+}
---
+++ Oh/4.4/TabExperiment/app/src/main/java/techtown/org/tabexperiment/TabFragment3.java
@@ -0,0 +1,28 @@
+package techtown.org.tabexperiment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TabFragment3 extends Fragment {
+
+
+
+    public TabFragment3() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab3, container, false);
+    }
+}
